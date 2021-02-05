72be86014c1c1aa014d6f51b5fe47a701f530397
kovalchuk-mykhailo <yakobiant@gmail.com>
2021-02-05 17:56:47 +0200

    Set up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-0001",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://qkf6pcg8q3.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_8y5VcFLDo",
+    APP_CLIENT_ID: "5hrs2cen254tf7scdh0o6rjnb6",
+    IDENTITY_POOL_ID: "us-east-1:1d0efe93-bcd7-46a6-b0b0-5640bac1696a",
+  },
+};
+
+export default config;
